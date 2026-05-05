946087217315326befc2cd2b11355d288d41d1f9
Uday Chavan <167866777+Udaychavan2307@users.noreply.github.com>
2026-05-05 12:24:07 +0530

    Classes and variables example using Student class

---
+++ Classes-Variables/main.java
@@ -0,0 +1,20 @@
+class Student {
+    // variables
+    String name;
+    int age;
+}
+
+public class Main {
+    public static void main(String[] args) {
+        // creating object
+        Student s1 = new Student();
+
+        // assigning values
+        s1.name = "Rahul";
+        s1.age = 20;
+
+        // printing directly (no method)
+        System.out.println("Name: " + s1.name);
+        System.out.println("Age: " + s1.age);
+    }
+}
