0f7a8f2924f94022bb9fa1fab357f303091c4030
Uday Chavan <167866777+Udaychavan2307@users.noreply.github.com>
2026-05-14 07:31:26 +0530

    Update License.java

--- Conditions/License.java
+++ Conditions/License.java
@@ -1,1 +1,23 @@
+import java.util.Scanner;
 
+class Licence {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter your age: ");
+        int age = sc.nextInt();
+
+        if (age > 75) {
+            System.out.println("You are age bar and not eligible for licence.");
+        }
+        else if (age >= 18) {
+            System.out.println("You are eligible for licence.");
+        }
+        else {
+            int years = 18 - age;
+            System.out.println("You are not eligible for licence.");
+            System.out.println("Wait for " + years + " years for licence.");
+        }
+    }
+}
